c88373bfe0a0e3bc3b87b6e6083fe38894536a85
Daniel Ayeni <ayenibabajidedaniel@gmail.com>
2022-09-28 22:58:44 +0100

    update in airtable

--- examples/3-airtable/app.js
+++ examples/3-airtable/app.js
@@ -1,1 +1,15 @@
 const result = document.querySelector('.result')
+
+
+const fetchData =  async () =>{
+try {
+ const { data } = await axios(` `)
+ console.log(data);
+
+} catch (error) {
+ 
+}
+
+}
+
+fetchData()
